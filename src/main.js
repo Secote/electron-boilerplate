16b6c764cba7648755bb901d16951f5812e71b01
Rao Frank <frankrao.usc@gmail.com>
2024-01-28 21:39:03 -0800

    Done developing auto starting server

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 // It doesn't have any windows which you can see on screen, but we can open
 // window from here.
 
-import { app, Menu, ipcMain, shell } from "electron";
+import { app, Menu, ipcMain, shell, net, BrowserWindow, ipcRenderer } from "electron";
 import appMenuTemplate from "./menu/app_menu_template";
 import editMenuTemplate from "./menu/edit_menu_template";
 import devMenuTemplate from "./menu/dev_menu_template";
@@ -23,24 +23,14 @@ if (env.name !== "production") {
   app.setPath("userData", `${userDataPath} (${env.name})`);
 }
 
+const APP_ENDPOINT = "http://localhost:8080";
 // Add this function to create overlay window
 const createOverlayWindow = (mainWindow) => {
-  const overlayWindow = createWindow("overlay", {
-    width: 200,
-    height: 200,
-    parent: mainWindow,
-    modal: true,
-    frame: false,
-    show: false,
-    webPreferences: {
-      enableRemoteModule: env.name === "production",
-      nodeIntegration: true
-    }
-  });
   const overlayFilePath = path.join(__dirname, "overlay.html");
+  const overlayWindow = new BrowserWindow({ parent: mainWindow, modal: false, transparent: true, show: false, center: true, frame: false, useContentSize: true, webPreferences: { nodeIntegration: true, defaultFontFamily: 'monospace' } });
+  overlayWindow.isMenuBarVisible = true;
+  overlayWindow.loadURL(overlayFilePath);
   console.log(`Overlay file path: ${overlayFilePath}`);
-  overlayWindow.loadURL(`file://${overlayFilePath}`);
-
   return overlayWindow;
 };
 
@@ -54,45 +44,95 @@ const setApplicationMenu = () => {
 };
 
 // We can communicate with our window (the renderer process) via messages.
-const initIpc = (mainWindow) => {
+const initIpc = () => {
   ipcMain.on("need-app-path", (event, arg) => {
     event.reply("app-path", app.getAppPath());
   });
   ipcMain.on("open-external-link", (event, href) => {
     shell.openExternal(href);
   });
-  const overlayWindow = createOverlayWindow(mainWindow);
-  ipcMain.on("show-overlay", () => {
-    overlayWindow.show();
-    console.log("Overlay window shown");
-  });
-
-  ipcMain.on("hide-overlay", () => {
-    overlayWindow.hide();
-    console.log("Overlay window hidden");
-  });
 };
 
+
+// make this a synchronous function so we can return a value
+function checkUrlValidity(url, timeout = 5000) {
+  return new Promise((resolve, reject) => {
+    const request = net.request(url);
+
+    // Set a timeout for the request
+    const requestTimeout = setTimeout(() => {
+      request.abort(); // Abort the request
+      resolve(false); // Resolve the promise with false
+    }, timeout);
+
+    request.on('response', (response) => {
+      clearTimeout(requestTimeout); // Clear the timeout
+
+      const statusCode = response.statusCode;
+
+      if (statusCode === 200) {
+        resolve(true); // Resolve the promise with true for a valid page
+      } else {
+        resolve(false); // Resolve the promise with false for an invalid page
+      }
+    });
+
+    request.on('error', (error) => {
+      clearTimeout(requestTimeout); // Clear the timeout
+      reject(error); // Reject the promise with the error
+    });
+
+    request.end();
+  });
+}
+
+async function startServerIfNecessary(mainWindow) {
+  const targetUrl = APP_ENDPOINT;
+  try {
+    const isUrlValid = await checkUrlValidity(targetUrl, 2000); // Specify timeout in milliseconds
+    console.log(`isUrlValid: ${isUrlValid}`);
+    if (isUrlValid) {
+      console.log(`${targetUrl} is valid`);
+      mainWindow.loadURL(targetUrl);
+      return;
+    }
+  } catch (error) {
+    console.error(`Error checking ${targetUrl}: ${error.message}`);
+  }
+  const overlayWindow = createOverlayWindow(mainWindow);
+  overlayWindow.once("ready-to-show", () => {
+    overlayWindow.show();
+  });
+  //window.electronAPI.showOverlay();
+  // Start the server if it's not running
+  const { exec } = require("child_process");
+  exec("docker-compose -f ./resources/build/docker-compose.app.yml up -d", (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error.message}`);
+      return;
+    }
+    console.log(`Script Output: ${stdout}`);
+    console.error(`Script Error: ${stderr}`);
+    //window.electronAPI.hideOverloay();
+  }).on('exit', (code) => {
+    console.log(`Child exited with code ${code}`);
+    overlayWindow.hide();
+    mainWindow.loadURL(targetUrl); });
+}
+
 app.on("ready", () => {
-  setApplicationMenu();
-
-
   const mainWindow = createWindow("main", {
     width: 1000,
     height: 600,
     webPreferences: {
       // Spectron needs access to remote module
-      enableRemoteModule: env.name === "production"
+      enableRemoteModule: env.name === "production",
+      preload: path.join(__dirname, 'preload.js')
     }
   });
-  initIpc(mainWindow);
-  mainWindow.loadURL(
-    "http://localhost:8080"
-  );
-
-  if (env.name === "development") {
-    mainWindow.openDevTools();
-  }
+  setApplicationMenu();
+  initIpc();
+  startServerIfNecessary(mainWindow);
 });
 
 app.on("window-all-closed", () => {
